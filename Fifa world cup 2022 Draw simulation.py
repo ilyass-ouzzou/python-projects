88a9a3f3890817588a1b08fc7aa3934b18c3d4de
Ilyass OUZZOU <73237624+ilyass-ouzzou@users.noreply.github.com>
2022-03-31 21:14:52 +0100

    Update Fifa world cup 2022 Draw simulation.py

--- Fifa world cup 2022 Draw simulation.py
+++ Fifa world cup 2022 Draw simulation.py
@@ -1,8 +1,11 @@
 import random
 def draw_simulation(group:list):
-    from_pot1 = random.choice(pot1)
-    group.append(from_pot1)
-    pot1.remove(from_pot1)
+    if(group == groupA):
+        pass
+    else:
+        from_pot1 = random.choice(pot1)
+        group.append(from_pot1)
+        pot1.remove(from_pot1)
     from_pot2 = random.choice(pot2)
     group.append(from_pot2)
     pot2.remove(from_pot2)
@@ -15,12 +18,13 @@ def draw_simulation(group:list):
     return group
 
 
-pot1 = ['Qatar', 'Brazil', 'Belgium', 'France', 'Argentina', 'England', 'Spain' , 'Portugal']
+pot1 = ['Brazil', 'Belgium', 'France', 'Argentina', 'England', 'Spain' , 'Portugal'] # Qatar is in the first Group by default
 pot2 = ['Mexico', 'Netherlands', 'Denmark', 'Germany', 'Uruguay', 'Switzerland', 'United States', 'Croatia']
 pot3 = ['Senegal', 'Iran', 'Japan', 'Morocco', 'Serbia', 'Poland', 'South Korea', 'Tunisia']
 pot4 = ['Cameroon', 'Canada', 'Ecuador', 'Saudi Arabia', 'Ghana', 'UEFA', 'AFC/CONMEBOL', 'CONCACAF/OFC']
 
 groupA = []
+groupA.append('Qatar')
 print("Group A : {}".format(draw_simulation(groupA)))
 groupB = []
 print("Group B : {}".format(draw_simulation(groupB)))
@@ -36,3 +40,18 @@ groupG = []
 print("Group G : {}".format(draw_simulation(groupG)))
 groupH = []
 print("Group H : {}".format(draw_simulation(groupH)))
+
+
+"""  
+Output:
+
+Group A : ['Qatar', 'Mexico', 'Iran', 'Saudi Arabia']
+Group B : ['Portugal', 'Switzerland', 'Serbia', 'UEFA']
+Group C : ['Spain', 'Croatia', 'Poland', 'Ghana']
+Group D : ['Belgium', 'Uruguay', 'South Korea', 'CONCACAF/OFC']
+Group E : ['Argentina', 'Netherlands', 'Tunisia', 'AFC/CONMEBOL']
+Group F : ['France', 'United States', 'Japan', 'Ecuador']
+Group G : ['Brazil', 'Germany', 'Morocco', 'Cameroon']
+Group H : ['England', 'Denmark', 'Senegal', 'Canada']
+
+"""
